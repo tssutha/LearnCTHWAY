5a2dc9324a18c07ec60c5aeafb59aaf6a751d506
tssutha <tssutha03@gmail.com>
2015-03-17 13:04:22 +0530

    first commit

---
+++ ex1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	puts("Hello World.");
+	puts("Hello Sri Lanka!");
+	puts("Hello C Language!");
+	puts("Hello Kitty ");
+	puts("Hello Mummy");
+	return 0;
+}
